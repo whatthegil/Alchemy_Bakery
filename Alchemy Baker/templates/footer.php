664167cb8e237b203c540b194bad3b7c17d5ba81
Gil Rexis Eleazar Realubit <145454819+whatthegil@users.noreply.github.com>
2024-12-01 00:21:39 +0800

    Add files via upload

---
+++ Alchemy Baker/templates/footer.php
@@ -0,0 +1,8 @@
+  <footer>
+    <div>
+        <p>📞 +639281414151 | ✉️ alchemybaker1@gmail.com |📍 Nabua, Camarines Sur | &copy; All Rights Reserved 2024</p>
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
---
+++ Alchemy Baker/templates/header.php
@@ -0,0 +1,69 @@
+<head>
+    <title>Alchemy Bakery</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style type="text/css">
+        .navbar-brand img {
+            margin-right: 10px;
+        }
+
+        .navbar-nav .nav-link {
+            font-weight: bold;
+            text-align: center;
+            justify-content: center;
+        }
+
+        .navbar-nav .nav-link.active {
+            color: #fff;
+            background-color: #E9B199;
+            border-radius: 5px;
+        }
+
+        .nav-link:hover {
+            color: #4CAF50;
+            background-color: #E9B199;
+            border-radius: 5px;
+        }
+        
+        footer {
+            background-color: #F0A483;
+            color: #000000;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            height: 10vh;
+        }
+    </style>
+</head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" alt="Alchemy Bakery Logo" width="100" height="80">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
